79642241a4d550f38f9d0340a72d66bb127631e8
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-10-08 20:49:57 -0500

    Estilo del componente de HeroScreen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams,Redirect } from 'react-router-dom';
 import { getHeroesById } from '../../selectors/getHeroeById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
 
     const { heroeId } = useParams();
 
@@ -12,16 +12,56 @@ export const HeroScreen = () => {
         return <Redirect to="/" />
     }
 
+    const handleReturn = () => {
+
+        //history.push('../');
+        if ( history.length <=2 ) {
+            history.push('/');
+        } else {
+            history.goBack();
+        }
+
+    }
+
     const {
         superhero,
         alter_ego,
         first_appearance,
         characters,
+        publisher
     } = hero;
 
     return (
-        <div>
-            <h1> Heroe Screen </h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img src={`../assets/heroes/${heroeId}.jpg`} alt={superhero} className="img-thumbnail"/>
+            </div>
+
+            <div className="col-8">
+                <h3>{ superhero }</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <b>Aleter ego: </b>
+                        { alter_ego }
+                    </li>
+                    <li className="list-group-item">
+                        <b>Publisher: </b>
+                        { publisher }
+                    </li>
+                    <li className="list-group-item">
+                        <b>First Appearance: </b>
+                        { first_appearance }
+                    </li>
+                </ul>
+                <h5>Characters</h5>
+                <p>{ characters }</p>
+                <button
+                    className="btn btn-outline-info"
+                    onClick={ handleReturn }
+                >
+                    Return
+                </button>
+            </div>
         </div>
     )
 }
