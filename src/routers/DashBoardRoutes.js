5e86df3cb52d14e11c5842b6b12de08cc3754b71
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-10-09 11:05:33 -0500

    Finalizando la parte de las vistas con su buscador

--- src/routers/DashBoardRoutes.js
+++ src/routers/DashBoardRoutes.js
@@ -4,6 +4,7 @@ import { DcScreen } from '../components/dc/DcScreen';
 import { HeroScreen } from '../components/heroes/HeroScreen';
 import { MarvelScreen } from '../components/marvel/MarvelScreen';
 import { Navbar } from '../components/ui/Navbar';
+import { SearchScreen } from '../components/search/SearchScreen';
 
 export const DashBoardRoutes = () => {
     return (
@@ -18,6 +19,8 @@ export const DashBoardRoutes = () => {
                     <Route exact path="/marvel" component={ MarvelScreen } />
                     <Route exact path="/hero/:heroeId" component={ HeroScreen } />
                     <Route exact path="/dc" component={ DcScreen } />
+                    <Route exact path="/search" component={ SearchScreen } />
+
 
                     <Redirect to="/marvel" />
 
